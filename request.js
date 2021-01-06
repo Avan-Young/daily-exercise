41b5bdc4363fe85e4d5b92e820046e35dd1afe65
Avan <yangshuiwang521@163.com>
2021-01-06 11:43:00 +0800

    视频区域、footer区域，以及axios请求的优化

---
+++ request.js
@@ -0,0 +1,14 @@
+// 相同域名
+axios.defaults.baseURL = "https://developer.duyiedu.com/vue/bz";
+
+axios.interceptors.response.use(response => {
+    const {
+        status
+    } = response;
+
+    if (status === 200) {
+        return response.data;
+    }
+
+    return response;
+});
